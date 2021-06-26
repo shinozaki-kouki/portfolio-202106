893d7fa5fcdd7e3411fec070af8f3b9945d4d808
shinozaki-kouki <shinozaki0605@icloud.com>
2021-06-26 13:45:45 +0900

    js修正

--- portfolio.js
+++ portfolio.js
@@ -16,40 +16,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
               }, 500);
               return false;
           });
-      });
-  
+  });
     const keyName = 'visited';
     const keyValue = true;
-  
   if (!sessionStorage.getItem(keyName)) {
-      //sessionStorageにキーと値を追加
       sessionStorage.setItem(keyName, keyValue);
-  
-      //ここに初回アクセス時の処理
       $('.fadeout').fadeIn("slow", function () {
         $(this).delay(5000).fadeOut("slow");
       });
       setTimeout(function(){
         $('.display').css('display','block');
    },6000);
-  
   } else {
-      //ここに通常アクセス時の処理
       $('.fadeout').css('display','none');
       $('.display').css('display','block');
   }
-  
-      $(function(){
-        $('.animation').css('visibility','hidden');
-        $(window).scroll(function(){
-         var windowHeight = jQuery(window).height(),
-             topWindow = jQuery(window).scrollTop();
-         $('.animation').each(function(){
-          var targetPosition = jQuery(this).offset().top;
-          if(topWindow > targetPosition - windowHeight + 100){
-           jQuery(this).addClass("fadeInUp");
-          }
-         });
-        });
-        });
+  $(function(){
+    $('.animation').css('visibility','hidden');
+    $(window).scroll(function(){
+      var windowHeight = jQuery(window).height(),
+          topWindow = jQuery(window).scrollTop();
+      $('.animation').each(function(){
+      var targetPosition = jQuery(this).offset().top;
+      if(topWindow > targetPosition - windowHeight + 100){
+        jQuery(this).addClass("fadeInUp");
+      }
+      });
+    });
+    });
   });
